635286d9818d35fa2f1910737143e1ca543d5649
Embtj <60002379+Embtj@users.noreply.github.com>
2025-06-20 17:10:11 -0500

    Set up initial header layout and styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h2>Genshin Impact</h2>
+      <h1>Character Archive</h1>
+      <div className="arrow-line">
+        <div className="arrow shaft-left"></div>
+        <div className="square"></div>
+        <div className="arrow shaft-right"></div>
+      </div>
+      
+    </div>
+  )
+}
